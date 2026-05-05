c0ee1e483ddf88a57e915d2e8594224517da7647
muanxng <meaningkasidis@gmail.com>
2026-05-05 19:15:39 +0700

    dog

--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -2,9 +2,6 @@ package chess;
 
 import chess.ui.ChessApp;
 
-/**
- * Entry point. Launches the JavaFX application.
- */
 public class Main {
     public static void main(String[] args) {
         ChessApp.main(args);
